a21ddb27d591cb84ab09bb18fdae50ec30ce3dcb
yu.kuai <yu.kuai@chaitin.com>
2025-07-22 02:48:36 +0000

    feat:(fe/admin): 版本型号

---
+++ web/admin/src/components/Sidebar/AuthTypeModal.tsx
@@ -0,0 +1,61 @@
+import HelpCenter from '@/assets/json/help-center.json';
+import IconUpgrade from '@/assets/json/upgrade.json';
+import { Box, Button, Stack } from "@mui/material";
+import { Modal } from "ct-mui";
+import LottieIcon from "../LottieIcon";
+
+interface AuthTypeModalProps {
+  open: boolean,
+  onClose: () => void,
+  curVersion: string,
+  latestVersion: string
+}
+
+const AuthTypeModal = ({ open, onClose, curVersion, latestVersion }: AuthTypeModalProps) => {
+  return <>
+    <Modal
+      open={open}
+      footer={null}
+      title='关于 PandaWiki'
+      onCancel={onClose}
+    >
+      <Stack gap={1} sx={{ fontSize: 14, lineHeight: '32px' }}>
+        <Stack direction={'row'} alignItems={'center'}>
+          <Box sx={{ width: 120, flexShrink: 0 }}>当前版本</Box>
+          <Stack direction={'row'} alignItems={'center'} gap={2}>
+            <Box sx={{ fontFamily: 'Gbold' }}>v{curVersion}</Box>
+            {latestVersion === `v${curVersion}` ? <Box sx={{ color: 'text.auxiliary', fontSize: 12 }}>
+              已是最新版本，无需更新
+            </Box> : <Button
+              size='small'
+              startIcon={<Box>
+                <LottieIcon
+                  id='version'
+                  src={latestVersion === '' ? HelpCenter : IconUpgrade}
+                  style={{ width: 16, height: 16, display: 'flex' }}
+                />
+              </Box>}
+              onClick={() => {
+                window.open('https://pandawiki.docs.baizhi.cloud/node/01971615-05b8-7924-9af7-15f73784f893')
+              }}
+            >
+              立即更新
+            </Button>}
+          </Stack>
+        </Stack>
+        <Stack direction={'row'} alignItems={'center'}>
+          <Box>产品型号</Box>
+          <Box></Box>
+        </Stack>
+        <Stack direction={'row'} alignItems={'center'}>
+          <Box>授权时间</Box>
+          <Box></Box>
+        </Stack>
+      </Stack>
+    </Modal>
+    <>
+    </>
+  </>
+};
+
+export default AuthTypeModal;
--- web/admin/src/components/Sidebar/Version.tsx
+++ web/admin/src/components/Sidebar/Version.tsx
@@ -5,10 +5,12 @@ import { Box, Stack, Tooltip } from "@mui/material";
 import { Icon } from 'ct-mui';
 import { useEffect, useState } from 'react';
 import packageJson from '../../../package.json';
+import AuthTypeModal from './AuthTypeModal';
 
 const Version = () => {
   const curVersion = import.meta.env.VITE_APP_VERSION || packageJson.version
   const [latestVersion, setLatestVersion] = useState<string | undefined>(undefined)
+  const [typeOpen, setTypeOpen] = useState(false)
 
   useEffect(() => {
     fetch('https://release.baizhi.cloud/panda-wiki/version.txt')
@@ -25,41 +27,46 @@ const Version = () => {
   if (latestVersion === undefined) return null
 
   return (
-    <Stack justifyContent={'center'} alignItems={'center'} gap={0.5} sx={{
-      borderTop: '1px solid',
-      borderColor: 'divider',
-      pt: 2,
-      mt: 1,
-      cursor: 'pointer',
-      color: 'text.primary',
-      fontSize: 12,
-      '&:hover': {
-        color: 'primary.main',
-      }
-    }} onClick={() => {
-      window.open('https://pandawiki.docs.baizhi.cloud/node/01971615-05b8-7924-9af7-15f73784f893')
-    }}>
-      <Stack direction={'row'} alignItems={'center'} gap={0.5}>
-        <Icon type='icon-banben' sx={{ fontSize: 16, color: 'success.main' }} />
-        免费版
+    <>
+      <Stack justifyContent={'center'} gap={0.5} sx={{
+        borderTop: '1px solid',
+        borderColor: 'divider',
+        pt: 2,
+        mt: 1,
+        cursor: 'pointer',
+        color: 'text.primary',
+        fontSize: 12,
+      }} onClick={() => setTypeOpen(true)}>
+        <Stack direction={'row'} gap={0.5}>
+          <Box sx={{ width: 30, color: 'text.auxiliary' }}>型号</Box>
+          <Icon type='icon-banben' sx={{ fontSize: 16, color: 'success.main' }} />
+          免费版
+        </Stack>
+        <Stack direction={'row'} gap={0.5}>
+          <Box sx={{ width: 30, color: 'text.auxiliary' }}>版本</Box>
+          <Box sx={{ whiteSpace: 'nowrap' }}>{curVersion}</Box>
+          {latestVersion !== `v${curVersion}` && <Tooltip
+            placement='top'
+            arrow
+            title={latestVersion === '' ? '无法获取最新版本' : '检测到新版本，点击查看'}
+          >
+            <Box>
+              <LottieIcon
+                id='version'
+                src={latestVersion === '' ? HelpCenter : IconUpgrade}
+                style={{ width: 16, height: 16 }}
+              />
+            </Box>
+          </Tooltip>}
+        </Stack>
       </Stack>
-      <Stack direction={'row'} alignItems={'center'} gap={0.5}>
-        <Box sx={{ whiteSpace: 'nowrap' }}>{curVersion}</Box>
-        {latestVersion !== `v${curVersion}` && <Tooltip
-          placement='top'
-          arrow
-          title={latestVersion === '' ? '无法获取最新版本' : '检测到新版本，点击查看'}
-        >
-          <Box>
-            <LottieIcon
-              id='version'
-              src={latestVersion === '' ? HelpCenter : IconUpgrade}
-              style={{ width: 16, height: 16 }}
-            />
-          </Box>
-        </Tooltip>}
-      </Stack>
-    </Stack>
+      <AuthTypeModal
+        open={typeOpen}
+        onClose={() => setTypeOpen(false)}
+        latestVersion={latestVersion}
+        curVersion={curVersion}
+      />
+    </>
   )
 }
 
